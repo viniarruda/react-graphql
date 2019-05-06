000b25d46e6259a1fc8a5a7196ee2d3c1ad00f3e
Vinícius Arruda <viniarruda.souza@gmail.com>
2019-05-06 14:21:17 -0300

    Update thunks.js

--- src/store/products/thunks.js
+++ src/store/products/thunks.js
@@ -9,8 +9,8 @@ export const testApi = () => async (dispatch, getState) => {
   dispatch(loadRequest())
   const response = await listGithub()
   
-  if(!response.data) {
-    const error = 'No have products'
+  if(!response) {
+    const error = 'No have response'
     dispatch(loadRejected(error))
 
     throw 'Error' 
